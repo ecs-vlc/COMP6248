db64a344e9f1838831641b9fc4e77bf6a1809e31
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2019-01-25 16:39:20 +0000

    bio lecture

---
+++ lecture-source/biological-inspiration/build.sh
@@ -0,0 +1,5 @@
+cp -r biological-inspiration ../../docs/lectures/
+git add ../../docs/lectures/biological-inspiration
+
+cp biological-inspiration-handouts.pdf ../../docs/handouts/
+git add ../../docs/handouts/
