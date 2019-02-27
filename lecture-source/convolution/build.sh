2a307922a7d42ae0e15db981680a38120cd816c5
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2019-02-27 09:34:46 +0000

    working on lectures

---
+++ lecture-source/convolution/build.sh
@@ -0,0 +1,5 @@
+# cp -r Convolution ../../docs/lectures/
+# git add ../../docs/lectures/Convolution
+
+cp Convolution-handouts.pdf ../../docs/handouts/
+git add ../../docs/handouts/
---
+++ lecture-source/image-network-archs/build.sh
@@ -0,0 +1,5 @@
+# cp -r Architectures ../../docs/lectures/
+# git add ../../docs/lectures/Architectures
+
+cp Architectures-handouts.pdf ../../docs/handouts/
+git add ../../docs/handouts/
