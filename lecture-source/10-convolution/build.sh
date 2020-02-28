d35bb8c0a6df25afb203cc174262b6cf19a87681
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2020-02-28 14:36:03 +0000

    update conv

--- lecture-source/10-convolution/build.sh
+++ lecture-source/10-convolution/build.sh
@@ -1,5 +1,5 @@
-# cp -r Convolution ../../docs/lectures/
-# git add ../../docs/lectures/Convolution
+cp -r Convolution.pdf ../../docs/lectures/
+git add ../../docs/lectures/Convolution.pdf
 
 cp Convolution-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
