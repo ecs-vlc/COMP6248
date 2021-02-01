4377d354bb87a419fedb79ed188dee26ba9b23c8
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-02-01 19:38:01 +0000

    updates

--- lecture-source/1-introduction/build.sh
+++ lecture-source/1-introduction/build.sh
@@ -5,3 +5,8 @@ git add ../../docs/lectures/
 latexmk -jobname=intro-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' intro
 cp intro-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh intro.pdf notwide
+    mv intro.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/1-intro.pptx
+fi
