421854fdcdbda82df62187f607bbc4023767f032
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-02-06 13:49:35 +0000

    update differentiation slides to ease presentation via video

--- lecture-source/3-differentiation/build.sh
+++ lecture-source/3-differentiation/build.sh
@@ -5,3 +5,8 @@ git add ../../docs/lectures/
 latexmk -jobname=differentiate-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' differentiate
 cp differentiate-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh differentiate.pdf notwide
+    mv differentiate.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/2-differentiate.pptx
+fi
