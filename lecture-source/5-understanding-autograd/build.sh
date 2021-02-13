c372c66b96a6b9eaa2cddcfeeb76942a64c23847
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-02-13 12:43:46 +0000

    typos and build scripts for 5 & 6

--- lecture-source/5-understanding-autograd/build.sh
+++ lecture-source/5-understanding-autograd/build.sh
@@ -5,3 +5,8 @@ git add ../../docs/lectures/
 latexmk -jobname=autograd-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' autograd
 cp autograd-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh autograd.pdf notwide
+    mv autograd.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/5-autograd.pptx
+fi
--- lecture-source/6-optimisation/build.sh
+++ lecture-source/6-optimisation/build.sh
@@ -5,3 +5,8 @@ git add ../../docs/lectures/
 latexmk -jobname=optimisation-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' optimisation
 cp optimisation-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh optimisation.pdf notwide
+    mv optimisation.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/6-optimisation.pptx
+fi
