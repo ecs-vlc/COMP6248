d937a4e67914a186adb57227353ebedc9325da6b
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-02-22 22:28:09 +0000

    changes to final slides

--- lecture-source/7-deepnetworks/build.sh
+++ lecture-source/7-deepnetworks/build.sh
@@ -5,3 +5,8 @@ git add ../../docs/lectures/
 latexmk -jobname=deepnetworks-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' deepnetworks
 cp deepnetworks-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh deepnetworks.pdf notwide
+    mv deepnetworks.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/7-deepnetworks.pptx
+fi
