6b4694cf5a2f1d84da078c73be9aaa7d567d2466
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-03-05 14:18:08 +0000

    update vid

--- lecture-source/10-convolution/build.sh
+++ lecture-source/10-convolution/build.sh
@@ -3,3 +3,8 @@ git add ../../docs/lectures/Convolution.pdf
 
 cp Convolution-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh convolution.pdf notwide
+    mv convolution.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/10-convolution.pptx
+fi
