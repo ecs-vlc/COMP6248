c0fcb01bdbb0b2b483036fbd1178ead89809401a
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-03-09 11:36:02 +0000

    update slides

--- lecture-source/11-12-image-network-archs/build.sh
+++ lecture-source/11-12-image-network-archs/build.sh
@@ -3,3 +3,8 @@
 
 cp Architectures-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh Architectures.pdf notwide
+    mv Architectures.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/11-12-architectures.pptx
+fi
