1fc9e7fec834f72c20fbcc784f084e96ff4a857f
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-03-17 13:34:16 +0000

    vids

--- lecture-source/13-rnns/build.sh
+++ lecture-source/13-rnns/build.sh
@@ -5,3 +5,8 @@ git add ../../docs/lectures/
 latexmk -jobname=rnns-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' rnns
 cp rnns-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh rnns.pdf notwide
+    mv rnns.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/13-rnns.pptx
+fi
--- lecture-source/14-lstm/build.sh
+++ lecture-source/14-lstm/build.sh
@@ -5,3 +5,8 @@ git add ../../docs/lectures/
 latexmk -jobname=lstm-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' lstm
 cp lstm-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh lstm.pdf notwide
+    mv lstm.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/14-lstm.pptx
+fi
