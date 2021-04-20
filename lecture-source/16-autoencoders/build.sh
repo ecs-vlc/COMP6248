bd165cbbeeac5573748399a1160e30fc201da5fd
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-04-20 10:05:24 +0100

    build

--- lecture-source/16-autoencoders/build.sh
+++ lecture-source/16-autoencoders/build.sh
@@ -5,3 +5,8 @@ git add ../../docs/lectures/
 latexmk -jobname=autoencoders-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' autoencoders
 cp autoencoders-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh autoencoders.pdf notwide
+    mv autoencoders.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/16-autoencoders.pptx
+fi
--- lecture-source/17-relaxations/build.sh
+++ lecture-source/17-relaxations/build.sh
@@ -5,3 +5,9 @@ git add ../../docs/lectures/
 latexmk -jobname=relaxation-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' relaxation
 cp relaxation-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh relaxation.pdf notwide
+    mv relaxation.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/17-relaxation.pptx
+fi
