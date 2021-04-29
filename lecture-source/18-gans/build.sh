143ffbd9b82f2ad87c24dfaf4e082e247c4a21b4
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2021-04-29 15:00:17 +0100

    links to vids

--- lecture-source/18-gans/build.sh
+++ lecture-source/18-gans/build.sh
@@ -5,3 +5,9 @@ git add ../../docs/lectures/
 latexmk -jobname=gans-handouts -pdf -pdflatex='pdflatex %O -interaction=nonstopmode -synctex=1 "\def\beamerclass{handout}\input{%S}"' gans
 cp gans-handouts.pdf ../../docs/handouts/
 git add ../../docs/handouts/
+
+
+if [[ -f "/Users/jsh2/Work/pdf2pptx/pdf2pptx.sh" ]]; then
+    /Users/jsh2/Work/pdf2pptx/pdf2pptx.sh gans.pdf notwide
+    mv gans.pdf.pptx /Users/jsh2/ownCloud/Teaching/COMP6248/slides/18-gans.pptx
+fi
